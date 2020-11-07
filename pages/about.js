b9ec057c8bb44ff6861ac90345ebba4bc137f2be
kiki <kevin.miguet@gmx.fr>
2020-11-07 13:39:49 +0100

    first working api

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,14 @@
-const about = () => (
-    <>About the good cup</>
+const About = ({ data }) => (
+    <>About the good coupe {data.text}</>
 )
-export default about
+
+export async function getStaticProps() {
+    // Fetch data from external API
+    const res = await fetch(`${process.env.API_URL}/beans`)
+    const data = await res.json()
+
+    // Pass data to the page via props
+    return { props: { data } }
+}
+
+export default About
---
+++ server/api/beans.js
@@ -0,0 +1,9 @@
+exports.handler = async (event, context) => {
+    return {
+        statusCode: 200,
+            headers: {
+                "content-type": "application/json"
+            },
+        body: JSON.stringify({text: "Hello, World"}),
+    };
+};
