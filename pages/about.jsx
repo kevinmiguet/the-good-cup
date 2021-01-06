10e1d9414dbbc3897bce14296bfaabe6167e7dd6
WHO <40888861+alphaWHO@users.noreply.github.com>
2021-01-06 21:13:44 +0100

    revert previous  exp on about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,16 +3,7 @@ const About = ({ data }) => (
     <h1>About the good coupe {data.text}</h1>
 )
 
-export async function getStaticProps(context) {
-        const res = await fetch(`https://.../beans`)
-        const data = await res.json()
-      
-        if (!data) {
-          return {
-            notFound: true,
-          }
-        }
-
+export async function getStaticProps() {
     // Pass data to the page via props
     return {
         props: {
