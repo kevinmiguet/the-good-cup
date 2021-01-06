caa143cce75e4f5f956921e234483abc56d2c108
WHO <40888861+alphaWHO@users.noreply.github.com>
2021-01-06 20:47:30 +0100

    exp on about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,12 +3,21 @@ const About = ({ data }) => (
     <h1>About the good coupe {data.text}</h1>
 )
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+        const res = await fetch(`https://.../beans`)
+        const data = await res.json()
+      
+        if (!data) {
+          return {
+            notFound: true,
+          }
+        }
+
     // Pass data to the page via props
     return {
         props: {
             data: {
-                text: 'hello good coupe is on the way!!'
+                text: 'hello good coupe is really on the way!!'
             }
         }
     }
