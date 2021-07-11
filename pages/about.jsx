916814fde7974f06e4abb91583213c966cd7611d
whoqui <who.moniscendo@gmail.com>
2021-07-11 23:38:01 +0200

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,9 +3,14 @@
 
 const About = ({ data }) => (
     <>
+    <title>A Worldwide community coffee lovers!</title>
     <header>A Worldwide community coffee lovers!</header>
     <h1>Having fun tantalising your taste buds with ze good coupe {data.text}</h1>
-    <span role="img" aria-label="sheep">🐑</span>
+    <span role="img" aria-label="sheep">🐑</span> <br></br>
+     
+     <div>
+         <button>Find your coffee</button> <button>Join the community</button>
+     </div>
     </>
 )
 
