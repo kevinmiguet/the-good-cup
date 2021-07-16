fbfa537fffd578949e90e09ac2108f1ef7f477be
whoqui <who.moniscendo@gmail.com>
2021-07-16 13:20:09 +0200

    navbar

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,15 +1,17 @@
+import Navbar from "../components/navbar";
 
 
 
 const About = ({ data }) => (
     <>
     <title>Worldwide community for coffee lovers!</title>
+    <Navbar/>
     <header>A worldwide community for coffee lovers!</header>
     <h1>Having fun tantalising your taste buds with ze good coupe {data.text}</h1>
     <span role="img" aria-label="cup of coffee">&#9749; 	</span> 
      
      <div>
-         <button>Find your coffee</button> <button>Join the community</button>
+         <button >Find your coffee</button> <button>Join the community</button>
      </div>
     </>
 )
