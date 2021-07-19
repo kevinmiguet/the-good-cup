0a965e271e60f34ae27386708cd93fc42225ca87
whoqui <who.moniscendo@gmail.com>
2021-07-19 13:26:32 +0200

    Update bean-info-card.jsx
    using tailwind

--- components/beans/bean-info-card.jsx
+++ components/beans/bean-info-card.jsx
@@ -1,11 +1,10 @@
-import {beanInfoCardContainer} from './bean-info-card.module.scss'
 import Like from "../like/like"
 import {Image} from '../commons/image'
 import StarRating from "../star/starRating"
 
 export const BeanInfoCard = ({bean}) => {
     
-    return (<div className={beanInfoCardContainer}>
+    return (<div className='mb-4 w-1/3'>
         <div>
             Country: {bean.country}
         </div>
@@ -15,9 +14,11 @@ export const BeanInfoCard = ({bean}) => {
         <div>
             Farm: {bean.farm}
         </div>
-       {bean.photos.map(function(photo) {
-        return <Image src={photo.url}/>
-       })} 
+        {bean?.photos?.length > 0 ? 
+            <Image className='h-52 w-52 object-cover' src={bean.photos[0].url}/> 
+            : null
+        }
+
        <Like/>
        <StarRating/>
        </div>)
