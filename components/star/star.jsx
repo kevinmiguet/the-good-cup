8e72fd9f8c0db129455d82df5b0fda2c61dc8cf6
whoqui <who.moniscendo@gmail.com>
2021-07-19 13:21:12 +0200

    cn star

--- components/star/star.jsx
+++ components/star/star.jsx
@@ -1,10 +1,11 @@
 
 import React from 'react'
 import {star,starSelected} from "./star.module.scss"
-
-
+import cn from 'classnames'
 const Star = ({ selected = false, onClick = f => f }) => (
-    <div className={selected ? 'starSelected' : 'star'} onClick={onClick} />
+    <div className={cn(star, selected ? starSelected : null)} onClick={onClick}> </div>
   );
 
+
+  
 export default Star
