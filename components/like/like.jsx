8ee88b890a3cf9088367145ff2e15dc5fd2b0049
whoqui <who.moniscendo@gmail.com>
2021-07-22 13:55:34 +0200

    like emoji

--- components/like/like.jsx
+++ components/like/like.jsx
@@ -13,8 +13,19 @@ const Like = ()=> {
 
     return (
         <div>
-        <button onClick={handleIncrement}>Like {likeCount}</button>
-        <button onClick={handleDecrement}>Dislike {dislikeCount}</button>
+        <button onClick={handleIncrement}>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+         {likeCount}
+        </button>
+
+        <button onClick={handleDecrement}>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>    
+        {dislikeCount}
+        </button>
         </div>
     );
 };
