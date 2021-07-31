1f3660f986296a01b19653870138496b7187a936
whoqui <who.moniscendo@gmail.com>
2021-07-31 18:57:10 +0200

    Update bean-info-card.jsx

--- components/beans/bean-info-card.jsx
+++ components/beans/bean-info-card.jsx
@@ -1,19 +1,24 @@
 import Like from "../like/like"
 import {Image} from '../commons/image'
 import StarRating from "../star/starRating"
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
+
 
 export const BeanInfoCard = ({bean}) => {
     
-    return (<div className='mb-4 w-1/3'>
-        <div>
+    return (<div className='mb-4 ml-1 mr-1 md:w-3/12 sm:w-1/3 w-full  bg-gray-200 rounded'>
+        <ul>
+        <li>
             Country: {bean.country}
-        </div>
-        <div>
+        </li>
+        <li>
             Drying Method: {bean.drying_method}
-        </div>
-        <div>
+        </li>
+        <li>
             Farm: {bean.farm}
-        </div>
+        </li>
+        </ul>
         {bean?.photos?.length > 0 ? 
             <Image className='h-52 w-52 object-cover' src={bean.photos[0].url}/> 
             : null
@@ -21,6 +26,12 @@ export const BeanInfoCard = ({bean}) => {
 
        <Like/>
        <StarRating/>
+       <div className="reactions">
+       <Picker
+       showPreview={false}
+        showSkinTones={false}
+        /> </div>
+
        </div>)
 }
 
