4aaf5d23df392487833bd2adc5a6491f08e7c352
whoqui <who.moniscendo@gmail.com>
2021-08-05 19:15:11 +0200

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,16 +2,20 @@ import Navbar from "../components/navbar";
 
 
 
+
 const About = ({ data }) => (
     <>
     <title>Worldwide community for coffee lovers!</title>
     <Navbar/>
+    <div className="bg-coffee-pattern bg-contain">
     <header>A worldwide community for coffee lovers!</header>
     <h1>Having fun tantalising your taste buds with ze good coupe {data.text}</h1>
     <span role="img" aria-label="cup of coffee">&#9749; 	</span> 
      
      <div>
-         <button >Find your coffee</button> <button>Join the community</button>
+         <button className="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">Find your coffee</button> 
+         <button className="bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">Join the community</button>
+     </div>
      </div>
     </>
 )
