72f8cf6eb354424b40835400979e94caa70dc844
whoqui <who.moniscendo@gmail.com>
2021-08-05 19:15:48 +0200

    Update tailwind.config.js
    bg coffee pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        'coffee-pattern':
+          "url('https://www.svgrepo.com/show/36388/coffee-bean.svg')",
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
